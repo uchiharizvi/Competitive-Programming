b0b120e4b5fce65e730ae83d30f254417d0c4137
Kavish Rizvi <rizvikavish205@gmail.com>
2021-09-17 15:47:43 +0530

    hackerank hourgalss problem

---
+++ src/HackerRank/Practice/Hourglass/Result.java
@@ -0,0 +1,11 @@
+package HackerRank.Practice.Hourglass;
+
+import java.util.List;
+
+public class Result {
+    public static int hourglassSum(List<List<Integer>> arr) {
+        // Write your code here
+
+        return 0;
+    }
+}
