6962fec8354e83ca9fe67d7f8254cae7898fe571
Kavish Rizvi <rizvikavish205@gmail.com>
2021-09-18 17:42:30 +0530

    added code

---
+++ src/HackerEarth/Practice/Easy/MaximumBorders/Solution.java
@@ -0,0 +1,16 @@
+package HackerEarth.Practice.Easy.MaximumBorders;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testCase = scan.nextInt();
+        for(int i=0;i<testCase;i++){
+            int rows = scan.nextInt();
+            int column = scan.nextInt();
+            //#-black cell, .-white cell
+
+        }
+    }
+}
